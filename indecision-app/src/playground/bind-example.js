21f3a6c4a3a4e7dfba640aab5c6a8a3bfca31ca7
Matt Smith <mattsmithereens@gmail.com>
2019-02-26 22:03:12 -0800

    bind example

---
+++ indecision-app/src/playground/bind-example.js
@@ -0,0 +1,11 @@
+const obj = {
+  name: 'Vikram',
+  getName() {
+    return this.name;
+  }
+}
+
+const getName = obj.getName.bind({ name: 'Andrew' });
+
+console.log(obj.getName());
+console.log(getName());
